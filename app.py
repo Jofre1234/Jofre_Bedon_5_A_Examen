b5ba6150030864d3c8b75b0c53be281558f6ef91
Jofre1234 <jjm.bedon@yavirac.edu.ec>
2025-11-28 15:55:51 -0500

    Examen Prueba final VI

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
-from flask import Flask, request, jsonify # type: ignore
-import pytest # type: ignore
-import sys
+from flask import Flask
 
 app = Flask(__name__)
 
-# Ruta principal
 @app.route('/')
-def hello_world():
-    return "¡buenos dias a todos!"
-
-
+def hello():
+    return "¡Buenos días a todos!"
 
 if __name__ == '__main__':
-    if "test" in sys.argv:
-        run_tests()  # type: ignore
-    else:
-        # CAMBIO REALIZADO: Puerto 1002
-        app.run(debug=True, host='0.0.0.0', port=1002)
+    app.run(host='0.0.0.0', port=1002, debug=True)
