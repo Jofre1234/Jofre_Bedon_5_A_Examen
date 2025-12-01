eaec948565e8d028ea93df8caaa3259e14dd2fac
Jofre1234 <jjm.bedon@yavirac.edu.ec>
2025-12-01 16:51:54 -0500

    Mi cambio V

--- app.py
+++ app.py
@@ -45,5 +45,3 @@ if __name__ == '__main__':
         run_tests()  # Ejecuta las pruebas con: python app.py test
     else:
         app.run(debug=True, host='0.0.0.0', port=5000)
-
-### Explicación del código:
